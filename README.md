673ae0d7d8e0147164ef61ed79a6d8e6924d9e3d
zhyncs <me@zhyncs.com>
2020-01-14 17:06:12 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wxbot
+
+Weather Bot
---
+++ wx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+map=(
+  '星期天'
+  '星期一'
+  '星期二'
+  '星期三'
+  '星期四'
+  '星期五'
+  '星期六'
+)
+week=$(echo ${map[$(date +%w)]})
+
+wx=$(curl -d "city=haidian&appkey=$APP_KEY" "https://way.jd.com/he/freeweather" | jq '.result.HeWeather5[0].daily_forecast[0]')
+
+date=$(echo ${wx} | jq '.date' | cut -d '"' -f2)
+
+txt_d=$(echo ${wx} | jq '.cond.txt_d' | cut -d '"' -f2)
+
+max=$(echo ${wx} | jq '.tmp.max' | cut -d '"' -f2)
+min=$(echo ${wx} | jq '.tmp.min' | cut -d '"' -f2)
+
+curl -X POST "https://api.telegram.org/bot$API_TOKEN/sendMessage" -d "chat_id=@$CHANNEL_ID&disable_notification=true&text=北京海淀%0A${date}%0A${week}%0A${txt_d}%0A${min}/${max}℃"
